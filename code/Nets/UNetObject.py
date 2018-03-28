3cc4f7edfd93d7704c90aed12ad96868e03e8ce4
pnaylor <peter.naylor@mines-paristech.fr>
2018-03-28 16:16:46 +0200

    update

--- code/Nets/UNetObject.py
+++ code/Nets/UNetObject.py
@@ -20,7 +20,7 @@ class UNet(DataReader):
                                                           self.IMAGE_SIZE[1],
                                                           self.BATCH_SIZE,
                                                           self.N_THREADS,
-                                                          3)
+                                                          self.NUM_CHANNELS)
         print("Queue initialized")
 
     def WritteSummaryImages(self):
@@ -41,7 +41,7 @@ class UNet(DataReader):
         self.image_PH = tf.placeholder_with_default(self.images_queue, shape=[None,
                                                                               None, 
                                                                               None,
-                                                                              3])
+                                                                              self.NUM_CHANNELS])
         return self.image_PH
 
     def conv_layer_f(self, i_layer, w_var, scope_name="conv", strides=[1,1,1,1], padding="VALID"):
